d50907e7ac86f486a80c8396b3d5c9e48355d8fa
smit1298 <61621188+smit1298@users.noreply.github.com>
2022-06-28 13:08:39 +0100

    project set up
    initial set up for all project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from "./Components/Sidebar/Dashboard/Dashboard";
+import Sidebar from "./Components/Sidebar/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <div className="grid md:grid-flow-row lg:grid-flow-col">
+      <Sidebar />
+      <Dashboard />
+     </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Sidebar = () => {
+  return <div>
+<h1 className="uppercase">retro</h1>
+</div>;
+};
+
+export default Sidebar;
